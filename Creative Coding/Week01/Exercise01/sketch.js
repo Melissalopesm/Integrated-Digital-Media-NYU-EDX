1a54ae4cf59251b38a5c2c76cd9d4ee43b1e20a1
melissa lopes <melissalopesm@yahoo.com.br>
2022-01-18 16:32:42 -0300

    update

--- Creative Coding/Week01/Exercise01/sketch.js
+++ Creative Coding/Week01/Exercise01/sketch.js
@@ -2,7 +2,7 @@ function setup() {
   print("Look, I can code!");
 }
 
-
+// Exemplo mostrado em aula. (Mouse desenha um elipse na tela)
 // function setup() {
 // 	createCanvas(windowWidth, windowHeight);
 // 	background(100);
