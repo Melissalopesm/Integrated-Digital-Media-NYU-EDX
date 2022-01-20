d4b86353ec23f63abb563c564efe5131d2c40dbc
melissa lopes <melissalopesm@yahoo.com.br>
2022-01-20 17:16:45 -0300

    safe commit

--- Creative Coding/Week02/Exercise01/sketch.js
+++ Creative Coding/Week02/Exercise01/sketch.js
@@ -1,11 +1,13 @@
+var bgRed = 100;
+
 function setup() {
   createCanvas(400, 400);
 }
 
 function draw() {
-  background(200, 24, 0);
-  fill(100, 12, 0);
+  background(bgRed, 24, 0);
+  fill(bgRed / 100, 12, 0);
   rect(0, 0, 100, 100);
 }
 
-//variables
+//variables exercises
