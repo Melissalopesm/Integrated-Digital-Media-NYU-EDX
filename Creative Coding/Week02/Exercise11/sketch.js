a74c9ed37ffb9047c2cdaa92d577b4fa3e7cd363
melissa lopes <melissalopesm@yahoo.com.br>
2022-01-26 10:24:47 -0300

    second week finished

--- Creative Coding/Week02/Exercise11/sketch.js
+++ Creative Coding/Week02/Exercise11/sketch.js
@@ -5,3 +5,12 @@ function setup() {
 function draw() {
   background(220);
 }
+
+
+Examples
+
+// "hello" + " " + "world" = Expression to "hello world"
+// 405.23 + 66 + sqrt(64) = Expression evaluates to 479.23
+// 64/3 = **Expression evaluates to 21**
+// 64 % 3 = Expression evaluates to 1 (reminder of 64/3)
+// If we want to say "every 10 frames" do: frame Count %10
